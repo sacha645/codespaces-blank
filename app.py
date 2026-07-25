c0e8541752d5d73283e5ebb25459bb3c9d1899d1
sacha645 <sachapollpay@gmail.com>
2026-07-25 11:55:12 +0000

    CVC4'

--- app.py
+++ app.py
@@ -29,8 +29,6 @@ def envoyer_code_email(email_destinataire, code):
 def init_db():
     conn = sqlite3.connect("utilisateurs.db")
     c = conn.cursor()
-    # ⚠️ AJOUTE CETTE LIGNE POUR REMETTRE À ZÉRO :
-    c.execute("DROP TABLE IF EXISTS users")
     c.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
