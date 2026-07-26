e05d51842ddcdce77eed83a63b4ebe9f198a459c
sacha645 <sachapollpay@gmail.com>
2026-07-26 14:34:41 +0000

    Test meilleur score 11

--- app.py
+++ app.py
@@ -1168,7 +1168,7 @@ elif st.session_state.etat == "connecte":
                             texte_score = "🏆 <b>Meilleur score :</b> Pas encore d'essai"
 
                         # Utilisation du CSS text-align: center pour tout centrer parfaitement
-                        st.markdown(f"<div style='text-align: center; color: gray; font-size: 0.85rem;'>{texte_score}</div>", unsafe_allow_html=True)
+                        st.markdown(f"<div style='text-align: left; color: gray; font-size: 1rem;'>{texte_score}</div>", unsafe_allow_html=True)
 
                         st.divider()
 
