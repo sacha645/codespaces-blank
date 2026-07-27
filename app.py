894dff21b2b344aac44cc2e8ad2448376de3bf70
sacha645 <sachapollpay@gmail.com>
2026-07-27 20:20:30 +0000

    Logique bouton site OP

--- app.py
+++ app.py
@@ -526,8 +526,7 @@ with col_title:
                         "total_vers_fr": st.session_state.get("total_vers_fr", 0.0),
                         "score_depuis_fr": st.session_state.get("score_depuis_fr", 0.0),
                         "total_depuis_fr": st.session_state.get("total_depuis_fr", 0.0),
-                        "erreurs_commises": st.session_state.get("erreurs_commises", []),
-                        "Temp" : f"Bizzare {type_liste}"
+                        "erreurs_commises": st.session_state.get("erreurs_commises", [])
                     }
                     
                     sauvegarder_partie(user_id, liste_id, etat_a_sauver)
@@ -543,8 +542,7 @@ with col_title:
                         "score_verbes": st.session_state.get("score_verbes", 0.0),
                         "total_verbes": st.session_state.get("total_verbes", 0.0),
                         "erreurs_compteur": st.session_state.get("erreurs_compteur", {}),
-                        "erreurs_verbes_detail": st.session_state.get("erreurs_verbes_detail", []),
-                        "Temp" : f"Le bug passe par la {type_liste}" 
+                        "erreurs_verbes_detail": st.session_state.get("erreurs_verbes_detail", [])
                     }
 
                     sauvegarder_partie(user_id, liste_id, etat_a_sauver)
@@ -1094,7 +1092,6 @@ elif st.session_state.etat == "connecte":
                 # 1. INITIALISATION / REPRISE DU QUIZ VOCABULAIRE
                 if "quiz_mots" not in st.session_state or st.session_state.get("quiz_liste_id") != liste_id:
                     partie_sauvee = charger_partie_sauvegardee(liste_id)
-                    st.write(partie_sauvee, type_liste, type_liste == "vocabulaire")
 
                     # Demande si une sauvegarde existe
                     if partie_sauvee and "choix_reprise" not in st.session_state:
