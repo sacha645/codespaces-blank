79d37edf0704d4c27262a2e0f485d8236311b1e7
sacha645 <sachapollpay@gmail.com>
2026-07-27 14:10:44 +0000

    Réinitialisation BDD sauf users

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ import random
 import json
 
 st.set_page_config(page_title="Réviseur", layout="wide")
+def reinitialiser_toutes_les_bdd():
+    conn = sqlite3.connect("utilisateurs.db")
+    c = conn.cursor()
+    # Supprime les 3 tables
+    c.execute("DROP TABLE IF EXISTS mots")
+    c.execute("DROP TABLE IF EXISTS listes")
+    c.execute("DROP TABLE IF EXISTS scores")
+    c.execute("DROP TABLE IF EXISTS sauvegardes_quiz")
+    conn.commit()
+    conn.close()
 
+# Décommente cette ligne, lance l'application une fois, puis recommente-la :
+reinitialiser_toutes_les_bdd()
 # --- BASE DE DONNÉES ---
 def init_db():
     conn = sqlite3.connect("utilisateurs.db")
