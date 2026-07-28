15589a4dabd352347b2e7fb0d918df5cd7570832
sacha645 <sachapollpay@gmail.com>
2026-07-28 14:28:24 +0000

    test 3"

--- app.py
+++ app.py
@@ -924,7 +924,7 @@ elif st.session_state.etat == "connecte":
                 st.session_state.mots_temp = {}
             st.session_state.id_a_suppr = None
             test=[]
-            test.append(st.session_state.id_a_suppr, "Début")
+            test.append((st.session_state.id_a_suppr, "Début"))
 
 
 
@@ -969,10 +969,10 @@ elif st.session_state.etat == "connecte":
                 with suppr :
                     if st.button("🗑️", key=i, help="Supprimer cette ligne"):
                         st.session_state.id_a_suppr = i
-                        test.append(st.session_state.id_a_suppr, "bouton")
+                        test.append((st.session_state.id_a_suppr, "bouton"))
 
             if st.session_state.id_a_suppr is not None : 
-                test.append(st.session_state.id_a_suppr, "test")
+                test.append((st.session_state.id_a_suppr, "test"))
                 if st.session_state.nb_lignes_mots > 2:
                     st.session_state.mots_temp.pop(st.session_state.id_a_suppr, None)
                     st.session_state.mots_temp = {(x if x < st.session_state.id_a_suppr else x - 1) : y for x, y in st.session_state.mots_temp.items()}
