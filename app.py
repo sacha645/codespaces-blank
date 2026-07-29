5f7d0c3b23d17a26b2b5847860a072047e79a71a
sacha645 <sachapollpay@gmail.com>
2026-07-29 22:15:12 +0000

    correctif visuel 17

--- app.py
+++ app.py
@@ -992,10 +992,10 @@ elif st.session_state.etat == "connecte":
                 if type_import_code == "verbe":
                     st.info("💡 **Format attendu :** `infinitif, présent, prétérit, participe passé, traduction en français`  \n(un verbe par ligne)")
                 else:
-                    st.info("💡 **Format attendu :** `article mot, traduction en français` (un mot par ligne)  \n\n"
+                    st.info("**Format attendu :** `article mot, traduction en français` (un mot par ligne)  \n\n"
                             "**IMPORTANT :**  \n"
                             "- L'article est facultatif  \n"
-                            "- Si une expression contient des espaces qui ne servent pas à séparer l'article du mot, il faut mettre un point d'exclamation (!) au début de la ligne.", icon=" ")
+                            "- Si une expression contient des espaces qui ne servent pas à séparer l'article du mot, il faut mettre un point d'exclamation (!) au début de la ligne.")
 
                 fichier_uploade = st.file_uploader("Choisis un fichier .txt", type=["txt"], accept_multiple_files=False)
 
