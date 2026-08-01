b3ee64593b8a3c65245900685b903277b1258392
sacha645 <sachapollpay@gmail.com>
2026-08-01 12:12:38 +0000

    test 16 liste

--- app.py
+++ app.py
@@ -1001,9 +1001,9 @@ elif st.session_state.etat == "none" or (st.session_state.etat == "connecte" and
             st.write("")
             
             st.markdown("""➢ Le site vous permet de créer deux types de listes selon vos besoins : 
-<ul>
-    <li><b>❖ Listes de Vocabulaire classique :</b> Pour apprendre des mots, des expressions ou des tournures de phrases...</li>
-    <li><b>❖ Listes de Verbes :</b> Spécialement conçues pour travailler toutes les formes des verbes : *Infinitif, Présent, Prétérit, Participe Passé, Traduction*. Idéal pour apprendre des verbes irrégulier par exemple !</li>
+<ul style="list-style-type: '❖ ';">
+    <li><b>Listes de Vocabulaire classique :</b> Pour apprendre des mots, des expressions ou des tournures de phrases...</li>
+    <li><b>Listes de Verbes :</b> Spécialement conçues pour travailler toutes les formes des verbes : *Infinitif, Présent, Prétérit, Participe Passé, Traduction*. Idéal pour apprendre des verbes irrégulier par exemple !</li>
 </ul>
 
 ➢ 🛠️ Comment créer une liste ?
