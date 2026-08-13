d3442a65fde56c50a3248096232a28450ab019c0
sacha645 <sachapollpay@gmail.com>
2026-08-13 15:23:41 +0000

    correctif 8.18'

--- app.py
+++ app.py
@@ -621,9 +621,6 @@ st.markdown(
     
     .centre {
     text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     justify-content: center;
     }
     </style>
