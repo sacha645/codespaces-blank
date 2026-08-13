35888f5c7ed5022afaf4df332227686fa55f63a1
sacha645 <sachapollpay@gmail.com>
2026-08-13 11:53:48 +0000

    correctif 8.11

--- app.py
+++ app.py
@@ -2254,11 +2254,11 @@ elif st.session_state.etat == "connecte":
                         st.success("🥳 **Nouveau meilleur score !**")
                     elif score_actuel_bdd:
                         anc_v, anc_tv, _, _ = score_actuel_bdd
-                        st.markdown(f"<center>🏆 <b>Meilleur score :</b> ⚡ <code>{anc_v:g}/{anc_tv:g}</code></center>", unsafe_allow_html=True)
+                        st.markdown(f"""<div style="text-align: center; color: #808495; font-size: 0.95rem; margin-bottom: 15px;">🏆 <b>Meilleur score :</b> ⚡ <b>{anc_v:g}/{anc_tv:g}</b></div>""", unsafe_allow_html=True)
 
                     st.write("")
 
-                    st.markdown(f"""<div style="text-align: center; margin: 10px 0;"><span style="font-size: 1em; color: white;">⚡ Score :</span><div style="font-size: 2rem; font-weight: 500;">{s_v:g} / {t_v:g}</div></div>""", unsafe_allow_html=True)
+                    st.markdown(f"""<div style="text-align: center; margin-top: 10px;"><div style="font-size: 1rem; color: #FFFFFF; font-weight: 500; margin-bottom: 4px;">⚡ Score :</div><div style="font-size: 2.2rem; font-weight: 700; color: #FFFFFF;">{s_v:g} / {t_v:g}</div></div>""", unsafe_allow_html=True)
 
                     # --- RECAPITULATIF DES ERREURS POUR VERBES ---
                     st.divider()
