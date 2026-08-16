85208dfe409c3d958c44758b4b2603a06e8c87a5
sacha645 <sachapollpay@gmail.com>
2026-08-16 17:39:29 +0000

    BDD permanante + rôle admin""

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ import json
 
 # --- BASE DE DONNÉES ---
 def get_connection():
-    return libsql.connect(url=st.secrets["TURSO_DATABASE_URL"], auth_token=st.secrets["TURSO_AUTH_TOKEN"])
+    return libsql.connect(database=st.secrets["TURSO_DATABASE_URL"], auth_token=st.secrets["TURSO_AUTH_TOKEN"])
 
 def init_db():
     conn = get_connection()
